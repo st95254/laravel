25ac298b972f15d9bf662142e7556c2fe85198de
st95254 <st890219@gmail.com>
2024-07-30 21:58:08 +0800

    modify meta data

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,11 +6,10 @@
     <link rel="icon" href="data:,">
 
     <meta name="description" content="灃耘生醫股份有限公司是專業的純黃金箔供應商，提供多種尺寸的高品質金箔和食用金箔。我們的產品獨家通過日本田中貴金屬的嚴格檢測，確保每一片金箔均達到最高標準。此外，我們還供應各種高品質的台灣茶葉，滿足您對金箔和茶葉的一切需求。">
-    <meta name="keywords" content="{{ $keywords ?? '灃耘, 金箔, 茶葉, GoldLeaf, GoldFoil, tea' }}">
+    <meta name="keywords" content="{{ $keywords ?? '灃耘, 灃耘股份有限公司, 灃耘生醫股份有限公司, 金箔, 茶葉, FengYun, z/ mp6, GoldLeaf, GoldFoil, tea' }}">
     <meta property="og:site_name" content="灃耘｜最高品質是不變的堅持" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://goldtea.458.com.tw/">
     <meta property="og:title" content="灃耘｜最高品質是不變的堅持"> 
     <meta property="og:description" content="灃耘生醫股份有限公司是專業的純黃金箔供應商，提供多種尺寸的高品質金箔和食用金箔。我們的產品獨家通過日本田中貴金屬的嚴格檢測，確保每一片金箔均達到最高標準。此外，我們還供應各種高品質的台灣茶葉，滿足您對金箔和茶葉的一切需求。"> 
-    <title>灃耘｜最高品質是不變的堅持</title>
 </head>
